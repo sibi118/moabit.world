2c01ac3a7709e0559720b35ca7c842d28b66a18f
Digipolis Moabit <anais@Anaiss-MacBook-Pro-2.local>
2021-02-03 16:25:22 +0100

    Akutualisierte Basiskarte
    Jetzt mit embedded Tileset

--- kneipe.tsx
+++ kneipe.tsx
@@ -1,4 +1,192 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="kneipe" tilewidth="32" tileheight="32" tilecount="210" columns="7">
- <image source="kneipe.png" width="224" height="960"/>
+<tileset version="1.4" tiledversion="1.4.3" name="kneipe" tilewidth="32" tileheight="32" tilecount="270" columns="9">
+ <image source="kneipe.png" width="288" height="960"/>
+ <terraintypes>
+  <terrain name="Text Beton" tile="70"/>
+ </terraintypes>
+ <tile id="69" terrain="0,0,0,0"/>
+ <tile id="70" terrain="0,0,0,0"/>
+ <tile id="78" terrain="0,0,0,0"/>
+ <tile id="79" terrain="0,0,0,0"/>
+ <tile id="97">
+  <properties>
+   <property name="collision" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="106">
+  <properties>
+   <property name="collision" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="145">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="148">
+  <properties>
+   <property name="collision" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="154">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="157">
+  <properties>
+   <property name="collision" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="159">
+  <properties>
+   <property name="collision" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="160">
+  <properties>
+   <property name="collision" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="167">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="168">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="169">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="170">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="176">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="177">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="178">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="179">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="185">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="186">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="187">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="188">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+  </properties>
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="0.14273" y="31.9001">
+    <polygon points="0,0 25.4059,0 24.835,-12.917 27.9037,-12.9884 27.8323,-31.7574 -0.285459,-31.8287"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="223">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="13.9875" y="32.0428">
+    <polygon points="0,0 0,-6.99376 -14.1302,-6.99376 -14.1302,-0.0713649"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="232">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="13.8448" y="0.14273">
+    <polygon points="0,0 0.0713649,32.0428 -13.9875,31.9715 -13.9161,-0.14273"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="234">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="235">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="236">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="237">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="238">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="239">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="240">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="241">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="10.7047" y="31.9715">
+    <polygon points="0,0 0.785013,-5.35236 0.713649,-14.5584 3.14005,-19.2685 3.06869,-31.9001 -10.6334,-31.9715 -10.7047,-0.214095"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="255">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="256">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="264">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="265">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+  </properties>
+ </tile>
 </tileset>
--- kneipenräume.tsx
+++ kneipenräume.tsx
@@ -1,4 +1,94 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="kneipenräume" tilewidth="32" tileheight="32" tilecount="400" columns="20">
  <image source="kneipenräume.png" width="640" height="640"/>
+ <tile id="60">
+  <properties>
+   <property name="collision" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="61">
+  <properties>
+   <property name="collision" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="62">
+  <properties>
+   <property name="collision" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="63">
+  <properties>
+   <property name="collision" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="64">
+  <properties>
+   <property name="collision" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="65">
+  <properties>
+   <property name="collision" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="80">
+  <properties>
+   <property name="collision" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="81">
+  <properties>
+   <property name="collision" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="82">
+  <properties>
+   <property name="collision" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="83">
+  <properties>
+   <property name="collision" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="84">
+  <properties>
+   <property name="collision" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="85">
+  <properties>
+   <property name="collision" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="100">
+  <properties>
+   <property name="collision" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="101">
+  <properties>
+   <property name="collision" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="102">
+  <properties>
+   <property name="collision" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="103">
+  <properties>
+   <property name="collision" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="104">
+  <properties>
+   <property name="collision" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="105">
+  <properties>
+   <property name="collision" type="bool" value="false"/>
+  </properties>
+ </tile>
 </tileset>
