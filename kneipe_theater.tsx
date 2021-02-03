5426e3dbddb5d25d897f6946897b9953564726b3
Digipolis Moabit <anais@Anaiss-MacBook-Pro-2.local>
2021-02-03 22:37:06 +0100

    Layer fix 2

---
+++ kneipe_theater.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="kneipe_theater" tilewidth="32" tileheight="32" tilecount="270" columns="9">
+ <image source="kneipe.png" width="288" height="960"/>
+</tileset>
