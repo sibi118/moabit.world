819c612c64d5c6884bd0807fabe14c0d65e597ce
Digipolis Moabit <anais@Anaiss-MacBook-Pro-2.local>
2021-02-04 01:52:14 +0100

    Mep Edit 1
    Diverse Details und Collision Test

--- kneipe_theater.tsx
+++ kneipe_theater.tsx
@@ -6,4 +6,43 @@
    <property name="jitsiRoom" value="mbitbar"/>
   </properties>
  </tile>
+ <tile id="93">
+  <properties>
+   <property name="exitUrl" value="kinotheater.json"/>
+  </properties>
+ </tile>
+ <tile id="94">
+  <properties>
+   <property name="exitUrl" value="kinotheater.json"/>
+  </properties>
+ </tile>
+ <tile id="102">
+  <properties>
+   <property name="exitUrl" value="kinotheater.json"/>
+  </properties>
+ </tile>
+ <tile id="103">
+  <properties>
+   <property name="exitUrl" value="kinotheater.json"/>
+  </properties>
+ </tile>
+ <tile id="174">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="183">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="192">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.19331" y="-0.338292" width="31.8478" height="13.435">
+    <properties>
+     <property name="collision" type="bool" value="false"/>
+    </properties>
+   </object>
+  </objectgroup>
+ </tile>
 </tileset>
